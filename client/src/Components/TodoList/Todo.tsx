0648933630d388aa117beaeceb45b4ec46ffdadf
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2023-02-14 21:14:25 +0200

    starting structure (todo category)

--- client/src/Components/TodoList/Todo.tsx
+++ client/src/Components/TodoList/Todo.tsx
@@ -1,5 +1,5 @@
 import './Todo.css'
-import { AiTwotoneDelete } from 'react-icons/ai';
+import { MdOutlineAddBox } from 'react-icons/md';
 import { FaEdit } from 'react-icons/fa';
 import { AiFillDelete } from 'react-icons/ai';
 import React, { useState } from 'react'
@@ -10,6 +10,12 @@ import Menu from '@mui/material/Menu';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import MoreIcon from '@mui/icons-material/MoreVert';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+// import { InputBase } from '@mui/material';
+import { Container } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 
 
 
@@ -37,6 +43,7 @@ const Todo = () => {
         setAnchorElUser(null);
     };
 
+    // start Todo Add function
     const handleAdding = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
         setTodolist([...todolist,
@@ -51,7 +58,9 @@ const Todo = () => {
         setDate('')
         setPhone('')
     }
+    // End Todo Add function
 
+    // start Todo Delete function
     const handleDeleting = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, index: number) => {
         e.preventDefault()
         Swal.fire({
@@ -74,7 +83,9 @@ const Todo = () => {
             }
         })
     }
+    // End Todo delete function
 
+    // start Todo Edit function
     const handleEditing = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, index: number) => {
         e.preventDefault()
         todolist.map((data, id) => {
@@ -90,63 +101,89 @@ const Todo = () => {
             return data
         })
     }
-
-    console.log(todolist);
+    // End Todo Edit function
 
 
     return (
         <div>
-            <form action="">
-                <input value={name} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setName(event.target.value)} type="text" placeholder='name' name="" id="" />
-                <input value={address} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setAddress(event.target.value)} type="text" placeholder='address' name="" id="" />
-                <input value={phone} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPhone(event.target.value)} type="text" placeholder='011..' name="" id="" />
-                <input value={date} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setDate(event.target.value)} type="date" name="" id="" />
-                <button onClick={handleAdding}>add</button>
-            </form>
-            <div>
-                {todolist.map((todo, index) => (
-                    <div key={index}>
-                        <p>{todo.name}</p>
-                        <p>{todo.address}</p>
-                        <p>{todo.date}</p>
-                        <p>{todo.phone}</p>
-                        {/* <button onClick={(e) => handleDeleting(e, index)}>delete</button> */}
-                        {/* <button onClick={(e) => handleEditing(e, index)}>edit</button> */}
-                        <Tooltip title="Open settings">
-                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <MoreIcon />
+            <Container>
+                <Row className='todo-row'>
+                    <Col className='col-1' xs={12} sm={12} md={6} lg={6}>
+                        <form className='form'>
+                            <p className='form-title'>Form</p>
+                            <input required value={name} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setName(event.target.value)} type="text" placeholder='Name' name="name" />
+                            <input required value={address} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setAddress(event.target.value)} type="text" placeholder='Address' name="address" />
+                            <input required value={phone} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPhone(event.target.value)} type="text" placeholder='01*********' name="telephone" />
+                            <input required value={date} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setDate(event.target.value)} type="date" name="date" />
+                            <IconButton onClick={handleAdding} sx={{ p: 0 }}>
+                                < MdOutlineAddBox color='#fff' size={30} />
                             </IconButton>
-                        </Tooltip>
-                        <Menu
-                            sx={{ mt: '45px' }}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
-                        >
-                            <MenuItem onClick={handleCloseUserMenu}>
-                                <IconButton onClick={(e) => handleEditing(e, index)} sx={{ p: 0 }}>
-                                    <Typography style={{marginRight:"4px"}}>Edit</Typography><FaEdit size={18} />
-                                </IconButton>
-                            </MenuItem>
-                            <MenuItem onClick={handleCloseUserMenu}>
-                                <IconButton onClick={(e) => handleDeleting(e, index)} sx={{ p: 0 }}>
-                                    <Typography>Delete</Typography><AiFillDelete size={18}  />
-                                </IconButton>
-                            </MenuItem>
-                        </Menu>
-                    </div>
-                ))}
-            </div>
+                        </form>
+                    </Col>
+                    <Col className='col-2' xs={12} sm={12} md={6} lg={6}>
+                        <div className='form-results'>
+                            {todolist.length === 0 ?
+                                <p className='text-no-items'>There are no items</p> :
+                                todolist.map((todo, index) => (
+                                    <div key={index} className="form-result" >
+                                        <div className='name-more-container'>
+                                            <div className='name'>
+                                                <label>Name:</label>
+                                                <p>{todo.name}</p>
+                                            </div>
+                                            <div className='more-icon'>
+                                                <Tooltip title="Open settings">
+                                                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                                        <MoreIcon />
+                                                    </IconButton>
+                                                </Tooltip>
+                                                <Menu
+                                                    sx={{ mt: '45px' }}
+                                                    id="menu-appbar"
+                                                    anchorEl={anchorElUser}
+                                                    anchorOrigin={{
+                                                        vertical: 'top',
+                                                        horizontal: 'right',
+                                                    }}
+                                                    keepMounted
+                                                    transformOrigin={{
+                                                        vertical: 'top',
+                                                        horizontal: 'right',
+                                                    }}
+                                                    open={Boolean(anchorElUser)}
+                                                    onClose={handleCloseUserMenu}
+                                                >
+                                                    <MenuItem onClick={handleCloseUserMenu}>
+                                                        <IconButton onClick={(e) => handleEditing(e, index)} sx={{ p: 0 }}>
+                                                            <Typography style={{ marginRight: "4px" }}>Edit</Typography><FaEdit size={18} />
+                                                        </IconButton>
+                                                    </MenuItem>
+                                                    <MenuItem onClick={handleCloseUserMenu}>
+                                                        <IconButton onClick={(e) => handleDeleting(e, index)} sx={{ p: 0 }}>
+                                                            <Typography>Delete</Typography><AiFillDelete size={18} />
+                                                        </IconButton>
+                                                    </MenuItem>
+                                                </Menu>
+                                            </div>
+                                        </div>
+                                        <div className='address'>
+                                            <label>Address:</label>
+                                            <p>{todo.address}</p>
+                                        </div>
+                                        <div className='phone'>
+                                            <label>Phone:</label>
+                                            <p>{todo.phone}</p>
+                                        </div>
+                                        <div className='date'>
+                                            <label>Date:</label>
+                                            <p>{todo.date}</p>
+                                        </div>
+                                    </div>
+                                ))}
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
