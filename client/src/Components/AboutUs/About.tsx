cfd6026e5fdec4c6ee0de19e3e3604c05d4014c6
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2023-02-22 23:05:39 +0200

    about us structure

--- client/src/Components/AboutUs/About.tsx
+++ client/src/Components/AboutUs/About.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import './About.css'
+import imgLeft from '../../assets/About us.svg'
 
 const About = () => {
   return (
-    <section id='about' style={{height:"30rem", backgroundColor:"#333"}}>
+    <div className="about-contain" id='about'>
+
+      <section className="about-content">
+        <h3>About Us</h3>
+        <div className="seperator"></div>
         <p>
-            About
-        </p>
-    </section>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Quas illum repudiandae aliquam consequatur explicabo accusantium ipsum?
+          Expedita pariatur delectus dicta harum corporis nisi,
+           assumenda aspernatur voluptate! Ipsa,
+          temporibus maxime! Rem!</p>
+      </section>
+      
+      <section className="img-side">
+        <img src={imgLeft} alt="" />
+      </section>
+    </div>
   )
 }
 
