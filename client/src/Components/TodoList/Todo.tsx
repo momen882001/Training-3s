590a479073d5df5ef73d4e159ccdf07a69031c0d
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2023-02-26 01:27:30 +0200

    finishing edit and add function

--- client/src/Components/TodoList/Todo.tsx
+++ client/src/Components/TodoList/Todo.tsx
@@ -3,7 +3,7 @@ import { IoIosAddCircle } from 'react-icons/io';
 import { FaEdit } from 'react-icons/fa';
 import { AiFillDelete } from 'react-icons/ai';
 import { TbArrowBigTop } from 'react-icons/tb';
-import React, { useRef, useState , useEffect} from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import Swal from 'sweetalert2'
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
@@ -32,7 +32,9 @@ const Todo = () => {
     const [date, setDate] = useState<string>('')
     const [phone, setPhone] = useState<string>('')
     const [todoId, setTodoId] = useState<number>(0)
+    const [editIndex, setEditIndex] = useState<number>(0)
     const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+    const [editFlag, setEditFlag] = useState<null | boolean>(false);
     let todoIndex = useRef<number | undefined>(undefined)
 
     const handleOpenUserMenu = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, index: number) => {
@@ -51,8 +53,27 @@ const Todo = () => {
     // start Todo Add function
     const handleAdding = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        if (todolist.length === 0) {
-            console.log("1st condition");
+        if (editFlag === true) {
+            console.log("dakhlt el edit");
+            let newTodoList = todolist[editIndex]
+            newTodoList.name = name
+            newTodoList.address = address
+            newTodoList.phone = phone
+            newTodoList.date = date
+            setName('')
+            setAddress('')
+            setDate('')
+            setPhone('')
+            setEditFlag(false)
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Your work has been edited',
+                showConfirmButton: false,
+                timer: 2500
+            })
+        } else {
+            console.log("continue push");
             setTodolist([...todolist,
             {
                 name: name,
@@ -66,39 +87,7 @@ const Todo = () => {
             setDate('')
             setPhone('')
             setTodoId(todoId + 1)
-        } else {
-            todolist.map((data) => {
-                if (data.id === undefined) {
-                    console.log("dakhlt el map");
-                    setTodolist([...todolist,
-                    {
-                        name: name,
-                        address: address,
-                        date: date,
-                        phone: phone,
-                        id: todoId
-                    }])
-                    setName('')
-                    setAddress('')
-                    setDate('')
-                    setPhone('')
-                    setTodoId(todoId + 1)
-                } else {
-                    console.log("edit box");
-                    console.log(data.id);
-                    console.log(todoId);
-                    let newTodoList = todolist[data.id]
-                    newTodoList.name = name
-                    newTodoList.address = address
-                    newTodoList.phone = phone
-                    newTodoList.date = date
-                    setName('')
-                    setAddress('')
-                    setDate('')
-                    setPhone('')
-                    setTodoId(todoId + 1)
-                }
-            })
+            setEditFlag(false)
         }
     }
     // End Todo Add function
@@ -140,99 +129,104 @@ const Todo = () => {
             }
             return data
         })
+        setEditFlag(true)
+        setEditIndex(index)
     }
     // End Todo Edit function
 
     useEffect(() => {
         AOS.init();
         AOS.refresh();
-      }, []);
+    }, []);
+
+    console.log(todolist)
 
 
     return (
         <>
-        <div id='todo' className="todo-contain">
-            <section className="todo-img">
-                <img src={imgLeft} alt="" />
-            </section>
-            <section className="todo-content">
-                <h3>Todo form</h3>
-                <div className="separator"></div>
-                <form className='form' onSubmit={handleAdding}>
-                    <input required value={name} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setName(event.target.value)} type="text" placeholder='Name' name="name" />
-                    <input required value={address} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setAddress(event.target.value)} type="text" placeholder='Address' name="address" />
-                    <input required value={phone} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPhone(event.target.value)} type="text" placeholder='01*********' name="telephone" />
-                    <input required value={date} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setDate(event.target.value)} type="date" name="date" />
-                    <button type='submit' className='add-btn'>Add <IoIosAddCircle size={15}/> </button>
-                </form>
-                <div className='form-results'>
-                    {todolist.length === 0 ?
-                        <p className='text-no-items'>There are no items</p> :
-                        todolist.map((todo, index) => (
-                            <div key={index} className="form-result" >
-                                <div className='name-more-container'>
-                                    <div className='name'>
-                                        <label>Name :</label>
-                                        <p>{todo.name}</p>
-                                    </div>
-                                    <div className='more-icon'>
-                                        <Tooltip style={{color:"#fff"}} title="Open options">
-                                            <IconButton onClick={(e) => handleOpenUserMenu(e, index)} sx={{ p: 0 }}>
-                                                <MoreIcon />
-                                            </IconButton>
-                                        </Tooltip>
-                                        <Menu
-                                            sx={{ mt: '45px' }}
-                                            id={index.toString()}
-                                            anchorEl={anchorElUser}
-                                            anchorOrigin={{
-                                                vertical: 'top',
-                                                horizontal: 'right',
-                                            }}
-                                            keepMounted
-                                            transformOrigin={{
-                                                vertical: 'top',
-                                                horizontal: 'right',
-                                            }}
-                                            open={todoIndex.current === index}
-                                            onClose={handleCloseUserMenu}
-                                        >
-                                            <MenuItem onClick={handleCloseUserMenu}>
-                                                <IconButton onClick={(e) => handleEditing(e, index)} sx={{ p: 0 }}>
-                                                    <Typography style={{ marginRight: "24px" }}>Edit</Typography><FaEdit size={18} />
+            <div id='todo' className="todo-contain">
+                <section className="todo-img">
+                    <img src={imgLeft} alt="" />
+                </section>
+                <section className="todo-content">
+                    <h3>Todo form</h3>
+                    <div className="separator"></div>
+                    <form className='form' onSubmit={handleAdding}>
+                        <input required value={name} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setName(event.target.value)} type="text" placeholder='Name' name="name" />
+                        <input required value={address} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setAddress(event.target.value)} type="text" placeholder='Address' name="address" />
+                        <input required value={phone} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPhone(event.target.value)} type="text" placeholder='01*********' name="telephone" />
+                        <input required value={date} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setDate(event.target.value)} type="date" name="date" />
+                        {editFlag === false ? <button type='submit' className='add-btn'>Add <IoIosAddCircle size={15} /></button>
+                            : <button type='submit' className='add-btn'>Edit <FaEdit size={17} /></button>}
+                    </form>
+                    <div className='form-results'>
+                        {todolist.length === 0 ?
+                            <p className='text-no-items'>There are no items</p> :
+                            todolist.map((todo, index) => (
+                                <div key={index} className="form-result" >
+                                    <div className='name-more-container'>
+                                        <div className='name'>
+                                            <label>Name :</label>
+                                            <p>{todo.name}</p>
+                                        </div>
+                                        <div className='more-icon'>
+                                            <Tooltip style={{ color: "#fff" }} title="Open options">
+                                                <IconButton onClick={(e) => handleOpenUserMenu(e, index)} sx={{ p: 0 }}>
+                                                    <MoreIcon />
                                                 </IconButton>
-                                            </MenuItem>
-                                            <MenuItem onClick={handleCloseUserMenu}>
-                                                <IconButton onClick={(e) => handleDeleting(e, index)} sx={{ p: 0 }}>
-                                                    <Typography style={{ marginRight: "4px" }}>Delete</Typography><AiFillDelete size={18} />
-                                                </IconButton>
-                                            </MenuItem>
-                                        </Menu>
+                                            </Tooltip>
+                                            <Menu
+                                                sx={{ mt: '45px' }}
+                                                id={index.toString()}
+                                                anchorEl={anchorElUser}
+                                                anchorOrigin={{
+                                                    vertical: 'top',
+                                                    horizontal: 'right',
+                                                }}
+                                                keepMounted
+                                                transformOrigin={{
+                                                    vertical: 'top',
+                                                    horizontal: 'right',
+                                                }}
+                                                open={todoIndex.current === index}
+                                                onClose={handleCloseUserMenu}
+                                            >
+                                                <MenuItem onClick={handleCloseUserMenu}>
+                                                    <IconButton onClick={(e) => handleEditing(e, index)} sx={{ p: 0 }}>
+                                                        <Typography style={{ marginRight: "24px" }}>Edit</Typography><FaEdit size={18} />
+                                                    </IconButton>
+                                                </MenuItem>
+                                                <MenuItem onClick={handleCloseUserMenu}>
+                                                    <IconButton onClick={(e) => handleDeleting(e, index)} sx={{ p: 0 }}>
+                                                        <Typography style={{ marginRight: "4px" }}>Delete</Typography><AiFillDelete size={18} />
+                                                    </IconButton>
+                                                </MenuItem>
+                                            </Menu>
+                                        </div>
+                                    </div>
+                                    <div className='address'>
+                                        <label>Address :</label>
+                                        <p>{todo.address}</p>
+                                    </div>
+                                    <div className='phone'>
+                                        <label>Phone :</label>
+                                        <p>{todo.phone}</p>
+                                    </div>
+                                    <div className='date'>
+                                        <label>Date :</label>
+                                        <p>{todo.date}</p>
                                     </div>
                                 </div>
-                                <div className='address'>
-                                    <label>Address :</label>
-                                    <p>{todo.address}</p>
-                                </div>
-                                <div className='phone'>
-                                    <label>Phone :</label>
-                                    <p>{todo.phone}</p>
-                                </div>
-                                <div className='date'>
-                                    <label>Date :</label>
-                                    <p>{todo.date}</p>
-                                </div>
-                            </div>
-                        ))}
-                </div>
-                <a href="#todo">
-                <div className="return-icon">
-                    <TbArrowBigTop size={27} style={{color:"#191919"}}/>
-                </div>
-                </a>
-            </section>
-        </div>
-                <div className="separator-section"></div>
+                            ))}
+                    </div>
+                    <a href="#todo">
+                        <div className="return-icon">
+                            <TbArrowBigTop size={27} style={{ color: "#191919" }} />
+                        </div>
+                    </a>
+                </section>
+            </div>
+            <div className="separator-section"></div>
         </>
     )
 }
